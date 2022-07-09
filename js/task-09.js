dcff951ee15fde6809a7026888f3a34b39f0b4e3
KarbanLVA <iam82@ukr.net>
2022-07-09 21:33:12 +0300

    +9 - виконано.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let randomColor = getRandomHexColor();
+console.log(randomColor);
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", onButtonClick);
+
+function onButtonClick (event) {
+  body.style.backgroundColor = randomColor;
+  span.textContent = `- ${randomColor}`;
 }
