bada67e5fd74d3f7dfb0fd1e6b4df78c333fe1f3
KarbanLVA <iam82@ukr.net>
2022-07-12 18:22:34 +0300

    зауваження виправлено.

--- js/task-09.js
+++ js/task-09.js
@@ -5,10 +5,12 @@ const span = document.querySelector(".color");
 
 button.addEventListener("click", onButtonClick);
 
-function onButtonClick (event) {
-  body.style.backgroundColor = getRandomHexColor();
-  span.textContent = `- ${getRandomHexColor()}`;
+function onButtonClick(event) {
+  const changeColor = getRandomHexColor()
+  body.style.backgroundColor = changeColor;
+  span.textContent = `- ${changeColor}`;  
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
